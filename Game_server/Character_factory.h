b8d390d06c5f30faf80ec71453722b3c3ef1fcb0
akemihomura0105 <zhang_fan0105@163.com>
2021-07-12 21:58:11 +0800

    update the gitignore

--- Game_server/Character_factory.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Character.h"
-#include <memory>
-class Character_factory
-{
-public:
-	static std::shared_ptr<Actionable_character> create(int character_id, int game_id, int session_id);
-};
--- miniGame/tools.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <iostream>
-inline void OTP(std::string str)
-{
-	std::cout << str << std::endl;
-}
-
